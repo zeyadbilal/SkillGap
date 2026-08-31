6f13cbb73d1c720d3acb2619f319de2af20f26ec
Ashraf Muhammed <bakr29420@gmail.com>
2026-08-31 23:04:18 +0300

    set up the be folders structure

---
+++ backend/src/middleware/errorHandler.js
@@ -0,0 +1,11 @@
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    status: "error",
+    message: err.message || "Internal Server Error",
+  });
+};
+
+module.exports = errorHandler;
