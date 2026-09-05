65608722e773d22bc06b146d9dc231634b2dd5ae
abdulrhman <abdulrhmanmokhtar399@gmail.com>
2026-09-05 23:15:30 +0300

    final

--- infra/main.tf
+++ infra/main.tf
@@ -98,6 +98,15 @@ module "eks" {
       max_size     = 3
       desired_size = 2
     }
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+    }
 
   }
 }
